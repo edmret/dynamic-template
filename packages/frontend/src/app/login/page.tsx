a93669f0b287e291939ee86f210928a304ba1985
Edmundo Retama <edmret@gmail.com>
2024-03-13 20:40:17 -0600

    Added front-end with login and register pages

---
+++ packages/frontend/src/app/login/page.tsx
@@ -0,0 +1,5 @@
+import LoginForm from "@/components/auth/LoginForm";
+
+export default function Login() {
+  return <LoginForm />;
+}
---
+++ packages/frontend/src/components/auth/RegisterForm.tsx
@@ -0,0 +1,105 @@
+"use client";
+import React from "react";
+import { Formik, Form } from "formik";
+import {
+  Box,
+  Button,
+  LinearProgress,
+  TextField,
+  Typography,
+} from "@mui/material";
+
+import * as Yup from "yup";
+
+const initialValues = {
+  username: "",
+  password: "",
+  confirmPassword: "",
+};
+
+const schema = Yup.object({
+  username: Yup.string().required("Required"),
+  password: Yup.string()
+    .required("Required")
+    .min(8, "Password must be at least 8 characters"),
+  confirmPassword: Yup.string()
+    .oneOf([Yup.ref("password"), null as any], "Passwords must match")
+    .required("Required"),
+});
+
+const RegistrationForm = () => {
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={schema}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 400);
+      }}
+    >
+      {({
+        submitForm,
+        isSubmitting,
+        touched,
+        errors,
+        handleChange,
+        values,
+      }) => (
+        <Form>
+          <Typography variant="h4" sx={{ marginY: 3 }}>
+            Register
+          </Typography>
+          <Typography variant="body1" sx={{ marginY: 3 }}>
+            Please enter your username and password to register.
+          </Typography>
+          <Box display="flex" flexDirection="column" gap={2}>
+            <TextField
+              onChange={handleChange}
+              name="username"
+              type="text"
+              label="Username"
+              fullWidth
+              helperText={touched.username ? errors.username : ""}
+              error={touched.username && Boolean(errors.username)}
+              value={values.username}
+            />
+            <TextField
+              onChange={handleChange}
+              name="password"
+              type="password"
+              label="Password"
+              fullWidth
+              helperText={touched.password ? errors.password : ""}
+              error={touched.password && Boolean(errors.password)}
+              value={values.password}
+            />
+            <TextField
+              name="confirmPassword"
+              type="password"
+              label="Confirm Password"
+              fullWidth
+              helperText={touched.confirmPassword ? errors.confirmPassword : ""}
+              error={touched.confirmPassword && Boolean(errors.confirmPassword)}
+              value={values.confirmPassword}
+              onChange={handleChange}
+            />
+            {isSubmitting && <LinearProgress />}
+            <Button
+              sx={{ marginTop: 2 }}
+              variant="contained"
+              color="primary"
+              disabled={isSubmitting}
+              onClick={submitForm}
+            >
+              Register
+            </Button>
+          </Box>
+        </Form>
+      )}
+    </Formik>
+  );
+};
+
+export default RegistrationForm;
